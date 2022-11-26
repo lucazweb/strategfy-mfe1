d0690ce96971d1c5238da15604ad0db7479b4a75
Lucas Cardoso Santos <lucas.casantos@b2wdigital.com>
2022-11-26 16:44:39 -0300

    chore: add react-application mfe1

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AppOne from "./components/AppOne";
+
+const App = () => (
+  <div>
+    <AppOne />
+  </div>
+);
+
+export default App;
---
+++ src/components/AppOne.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppOne: React.FC = () => {
+  return (
+    <header>
+      <h1>Header App1</h1>{" "}
+    </header>
+  );
+};
+
+export default AppOne;
